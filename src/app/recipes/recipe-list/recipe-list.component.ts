5a6e3fdb49f27912f8bd98938d9d1867192ecf18
ParameshA23 <parameshanthati18@gmail.com>
2023-03-05 18:27:38 +0530

    Initial Commit

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent {
+  recipes: Recipe[]=[
+    new Recipe("Test","taste Test","https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F44%2F2022%2F03%2F01%2Fcucumber-sandwich.jpg")
+  ];
+  constructor() {}
+  ngOnInit() {
+
+  }
+}
